693795439bf6a7b2b676f045887a6f84419ce198
shubham tiwari <st1173076@gmail.com>
2023-11-25 18:08:29 +0530

    fix: percentage value

--- sass/script.js
+++ sass/script.js
@@ -9,14 +9,21 @@ var elibilityEl = document.getElementById("eligibility")
 
  
 inputEl.addEventListener("input",()=>{
-    number.textContent = inputEl.value + "%";
-    marksEl.style.strokeDashoffset = (450 - (450*inputEl.value)/100);
-    if(inputEl.value >= 40){
+    if( inputEl.value <= 100 && inputEl.value > 40 ){
+        number.textContent = inputEl.value + "%";
+        marksEl.style.strokeDashoffset = (450 - (450*inputEl.value)/100);
         elibilityEl.innerHTML = `<p><i class="fas fa-check"></i> you are eligible</p>`
         elibilityEl.style.color = "green"
-    }else{
+    }else if(inputEl.value > 0 && inputEl.value < 40 ){
+        number.textContent = inputEl.value + "%";
+        marksEl.style.strokeDashoffset = (450 - (450*inputEl.value)/100);
         elibilityEl.innerHTML = `<p><i class="fas fa-times"></i> you are not eligible</p>`
         elibilityEl.style.color = "red"
+    }else{
+        number.textContent =  "100%";
+        marksEl.style.strokeDashoffset = (450 - (450*100)/100);
+
+
     }
 })
 
